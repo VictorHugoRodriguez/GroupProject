bde70e134e3e3b57f43c5c7148e4059ea2128c25
dramirez.c90 <david.ramirez@motionminers.de>
2018-11-13 08:20:07 +0100

    added gitignore and added flask-socketio for real time connection to database

--- server.py
+++ server.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World'
+app = Flask(__name__)
+token = "ajkjd77sdlkjf&&8f"
+
+@app.route('/login', methods=['POST'])
+def index():
+    user = request.form['user']
+    pwd = request.form['password']
+    print(user)
+    if(user == "admin" & pwd == "admin" ):
+        user: {
+            "name": "David Ramirez",
+            "company": "Motion Miners",
+            "token": token
+        }
+        response = jsonify(user)
+        response.status_code = 200
+    else:
+        response.status_code = 401
+    return response
+
+
